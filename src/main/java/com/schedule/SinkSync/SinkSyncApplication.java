68ef2d708bdd37e8a7cbdd2623a7cab9e443201d
Gherman Larisa <gherman.larisa3@gmail.com>
2026-07-28 08:12:34 +0300

    chore: initialise Spring Boot project

---
+++ src/main/java/com/schedule/SinkSync/SinkSyncApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.SinkSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinkSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinkSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedule/SinkSync/SinkSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule.SinkSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinkSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
